7558e11e6b17d3182e3dfdfd449f50cdedf9d623
superlintball <ravnikator@gmail.com>
2017-09-29 12:26:03 -0700

    Created ADD and PRINT functions

--- studentList.cpp
+++ studentList.cpp
@@ -17,19 +17,45 @@ struct Student
 	float gpa;
 };
 
-&Student addStudent()
+void addStudent(vector<Student*> &stList)
 {
-	Student person;
+	stList.push_back(new Student());
 	cout << "Enter student's first name." << endl;
-	cin >> person.firstName;
+	cin >> stList.back()->firstName;
 	cout << "Enter student's last name." << endl;
-	cin >> person.lastName;
+	cin >> stList.back()->lastName;
 	cout << "Enter student's ID Number." << endl;
-	cin >> person.id;
+	cin >> stList.back()->id;
 	cout << "Enter student's GPA." << endl;
-	cin >> person.gpa;
-	return &person;
+	cin >> stList.back()->gpa;
 };
+
+void printStudents(vector<Student*> stList)
+{
+	std::vector<Student*>::iterator it;
+	for(it = stList.begin(); it != stList.end(); it++)
+	{
+		// idea from the C++ Forum, answer from user JLBorges
+		// http://www.cplusplus.com/forum/general/71342/
+		// accesses object position by taking the difference between the iterator and the beginning
+		int position = it - stList.begin();
+
+		cout << stList[position]->firstName << " " << stList[position]-> lastName;
+		cout << ", " << stList[position]->id << ", " << stList[position]->gpa << endl;
+	}
+}
+
+void deleteStudent(vector<Student*> &stList)
+{
+	cout << "Enter ID number of student to be deleted." << endl;
+	int id;
+	cin >> id;
+	std::vector<Student*>::iterator it;
+	for(it = stList.begin(); it != stList.end(); it++)
+	{
+		int position = it - stList.begin();
+	}
+}
 
 //main code block
 int main()
@@ -44,15 +70,23 @@ int main()
 		cout << "\"ADD\", \"PRINT\", or \"DELETE\" student(s)?" << endl;
 		cout << "Type \"QUIT\" to exit." << endl;
 		cin >> input;
-		if(strcmp(input, "ADD") == 0)
+		if(strcmp(input, "ADD") == 0 || strcmp(input, "add") == 0)
 		{
-			studentList.push_back(addStudent());
-		} else if(strcmp(input, "QUIT") == 0)
+			addStudent(studentList);
+			cout << "Student Added." << endl << endl;
+		} else if(strcmp(input, "PRINT") == 0 || strcmp(input, "print") == 0)
+		{
+			printStudents(studentList);
+			cout << "All Students Printed." << endl << endl;
+		} else if(strcmp(input, "DELETE") == 0 || strcmp(input, "delete") == 0)
+		{
+			deleteStudent(studentList);
+		} else if(strcmp(input, "QUIT") == 0 || strcmp(input, "quit") == 0)
 		{
 			keepGoing = false;
 		} else
 		{
-			cout << "Sorry, I couldn't understand that." << endl;
+			cout << "Sorry, I couldn't understand that." << endl << endl;
 		}
 	}
 	return 0;
