8aad4d35302ecd2413dd6e001bbd640d57590e2d
superlintball <ravnikator@gmail.com>
2017-10-03 11:59:52 -0700

    Finished Student List

--- studentList.cpp
+++ studentList.cpp
@@ -1,6 +1,6 @@
 /*
  * Author Raveen Karnik
- * Date: 9-21-17
+ * Date: 10-3-17
  * This program allows user to add and remove students from a list of students
  */
 
@@ -9,6 +9,7 @@
 #include <vector>
 using namespace std;
 
+//struct to define student
 struct Student
 {
 	char firstName[80];
@@ -17,9 +18,13 @@ struct Student
 	float gpa;
 };
 
+//add student function
 void addStudent(vector<Student*> &stList)
 {
+	//add new student to end of vector
 	stList.push_back(new Student());
+
+	//prompt user for all information on student
 	cout << "Enter student's first name." << endl;
 	cin >> stList.back()->firstName;
 	cout << "Enter student's last name." << endl;
@@ -28,10 +33,12 @@ void addStudent(vector<Student*> &stList)
 	cin >> stList.back()->id;
 	cout << "Enter student's GPA." << endl;
 	cin >> stList.back()->gpa;
-};
+}
 
+//go through all students and print their information
 void printStudents(vector<Student*> stList)
 {
+	//defint iterator to go through vector
 	std::vector<Student*>::iterator it;
 	for(it = stList.begin(); it != stList.end(); it++)
 	{
@@ -45,46 +52,82 @@ void printStudents(vector<Student*> stList)
 	}
 }
 
-void deleteStudent(vector<Student*> &stList)
+//delete student function; boolean so user can know if the student was deleted
+bool deleteStudent(vector<Student*> &stList)
 {
+	//prompt user for id of student to be deleted, then iterate through
 	cout << "Enter ID number of student to be deleted." << endl;
-	int id;
-	cin >> id;
+	int deleteId;
+	cin >> deleteId;
 	std::vector<Student*>::iterator it;
 	for(it = stList.begin(); it != stList.end(); it++)
 	{
+		//check each student in the vector for a matching id, and delete if found
 		int position = it - stList.begin();
+		if(stList[position]->id == deleteId)
+		{
+			//delete student and return true to indicate the student was deleted
+			delete stList[position];
+			stList.erase(stList.begin()+position);
+			return true;
+		}
 	}
+	//return false if no matching student was found
+	return false;
 }
 
 //main code block
 int main()
 {
+	//define variables to be used
 	vector<Student*> studentList;
-	vector<Student*>* listPtr = &studentList;
 	studentList.reserve(10);
 	char input[8];
+
+	//general loop to stay true until the user signals to quit
 	bool keepGoing = true;
 	while(keepGoing)
 	{
+		//prompt user to add, print, or delete student(s), calling the corresponding function
 		cout << "\"ADD\", \"PRINT\", or \"DELETE\" student(s)?" << endl;
 		cout << "Type \"QUIT\" to exit." << endl;
 		cin >> input;
+
+		//if the user wants to add a student, add that student and print that they have been added
 		if(strcmp(input, "ADD") == 0 || strcmp(input, "add") == 0)
 		{
 			addStudent(studentList);
 			cout << "Student Added." << endl << endl;
-		} else if(strcmp(input, "PRINT") == 0 || strcmp(input, "print") == 0)
+		}
+		
+		//if the user wants to prints students, print them all and notify when complete
+		else if(strcmp(input, "PRINT") == 0 || strcmp(input, "print") == 0)
 		{
 			printStudents(studentList);
 			cout << "All Students Printed." << endl << endl;
-		} else if(strcmp(input, "DELETE") == 0 || strcmp(input, "delete") == 0)
+		} 
+		
+		//if the user wants to delete a student, attempt to delete that student
+		else if(strcmp(input, "DELETE") == 0 || strcmp(input, "delete") == 0)
 		{
-			deleteStudent(studentList);
-		} else if(strcmp(input, "QUIT") == 0 || strcmp(input, "quit") == 0)
+			//depending on what deleteStudent returns, notify the user of what happened
+			if(deleteStudent(studentList))
+			{
+				cout << "Selected Student Deleted." << endl << endl;
+			} else
+			{
+				cout << "Given Student was not Found." << endl << endl;
+			}
+		}
+		
+		//if the user wants to quit, set the boolean to false, closing the while loop
+		else if(strcmp(input, "QUIT") == 0 || strcmp(input, "quit") == 0)
 		{
 			keepGoing = false;
-		} else
+		}
+		
+		//indicate confusion with any other input
+		else
 		{
 			cout << "Sorry, I couldn't understand that." << endl << endl;
 		}
